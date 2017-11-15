56cb0d643d135c831a01f0b12235fcc6a58cee36
Mickey <mj@fortknoxster.com>
2017-11-15 01:35:11 +0100

    Comments updated

--- contracts/FKXCrowdsale.sol
+++ contracts/FKXCrowdsale.sol
@@ -8,9 +8,8 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
  * @title FKXCrowdsale
- * @dev This is an example of a fully fledged crowdsale.
- * The way to add new features to a base crowdsale is by multiple inheritance.
- * In this example we are providing following extensions:
+ * @dev The FKX crowdsale based
+ * Inheritance:
  * TokenCappedCrowdsale - sets a max boundary in FKX token for raised funds (from zeppelin-solidity's CappedCrowdsale.sol)
  * FinalizableCrowdsale - makes the crowdsale finalizable
  *
