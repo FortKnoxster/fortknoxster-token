74e2d902d6265794a2fedcdcb6a34730d4c75be5
Mickey <mj@fortknoxster.com>
2017-11-24 16:02:14 +0100

    Various updates

---
+++ contracts/FKX.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.18;
+
+import "zeppelin-solidity/contracts/token/PausableToken.sol";
+import "zeppelin-solidity/contracts/token/CappedToken.sol";
+
+
+/**
+ * @title FKX
+ */
+contract FKX is PausableToken, CappedToken(FKX.TOKEN_SUPPLY) {
+
+  using SafeMath for uint256;
+
+  string public constant name = "Knoxstertoken";
+  string public constant symbol = "FKX";
+  uint8 public constant decimals = 18;
+  string public constant version = "1.0";
+  uint256 public constant TOKEN_SUPPLY  = 135000000 * (10 ** uint256(decimals)); // 135 Million FKX
+
+  function FKX() public {
+   
+  }
+
+}
