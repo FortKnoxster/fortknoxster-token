78a550f24469db8be67d60191fbf863ff34357cb
Mickey <mj@fortknoxster.com>
2018-01-19 11:28:31 +0100

    updated migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,13 +4,17 @@ var FKX = artifacts.require("./FKX.sol");
 
 var FKXSale = artifacts.require('./FKXSale.sol');
 
-console.log("FKX address: " + FKX.address);
+console.log("Deploying contracts...");
 
 module.exports = function (deployer, network, accounts) {
+    
+    if (accounts.length) {
+        for (var i = 0; i < accounts.length; i++) {
+            console.log("Acount:  " + accounts[i]);
+        }
+    }
     
     deployer.deploy(FKX).then(function() {
         return deployer.deploy(FKXSale, FKX.address);
     });
-    
-
 };
